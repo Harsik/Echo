9c936c51f57dc15176746a1a570efce0b3c6c62b
aznm12 <aznm12@archivsoft.com>
2019-06-19 16:07:33 +0900

    setAvatarUrl

--- src/main/java/echo/controller/ChatController.java
+++ src/main/java/echo/controller/ChatController.java
@@ -1,6 +1,9 @@
 package echo.controller;
 
 import echo.model.ChatMessage;
+import echo.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,9 +13,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
+    @Autowired
+    private AccountService accountService;
+    
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatMessage.setAvatarUrl(accountService.loadAvatarByEmail(chatMessage.getSender()).getDownloadUri());
         return chatMessage;
     }
 
